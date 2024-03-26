b9e638bef41105af906066675ad89730618856f3
GrufasXd <147091694+GrufasXd@users.noreply.github.com>
2024-03-26 20:55:22 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <ctime>
 using namespace std;
+
 int main() {
     srand(time(nullptr));
 
@@ -13,41 +14,51 @@ int main() {
     vector<double> median;
     int kodas;
     int rusis;
-    //createfile("failas1.txt", 1000);
-    //studrus(students, galrez, vargsai, galva, "failas1.txt");
-    //createfile("failas2.txt", 10000);
-    //createfile("failas3.txt", 100000);
-    //createfile("failas4.txt", 1000000);
-    //createfile("failas5.txt", 10000000);
-    try {
-        cout << "Ar norite duomenis generuoti / rasyti ranka - 1" << endl << "Ar norite duomenis skaityti is failo - 2" << endl;
-        cin >> kodas;
+    bool correctInput = false;
 
-        if (kodas != 1 && kodas != 2) {
-            throw "Neteisingas ivesties pasirinkimas.";
-        }
+    while (!correctInput) {
+        try {
+            cout << "Ar norite duomenis generuoti / rasyti ranka - 1" << endl << "Ar norite duomenis skaityti is failo - 2" << endl;
+            cin >> kodas;
 
-        if (kodas == 1) {
-            rasytiranka(students, galrez, median);
-        } else if (kodas == 2) {
-            skaitymas(students, galrez, median);
-            int slap;
-            try {
-                cout << "Ar duomenis spausdinti ekrane - 1" << endl << "Ar duomenis spausdint faile - 2" << endl;
-                cin >> slap;
-                if (slap == 1)
-                    spausdint(students, galrez, median);
-                else if (slap == 2)
-                    spausdintfaila(students, galrez, median);
-                else
-                    throw "Neteisingas pasirinkimas duomenu spausdinimui.";
-            } catch (const char* msg) {
-                cerr << msg << endl;
-                return 1;
+            if (kodas != 1 && kodas != 2) {
+                throw "Neteisingas ivesties pasirinkimas.";
             }
+            correctInput = true;
+
+            if (kodas == 1) {
+                rasytiranka(students, galrez, median);
+            } else if (kodas == 2) {
+                skaitymas(students, galrez, median);
+                int slap;
+                bool correctPrintInput = false;
+
+                while (!correctPrintInput) {
+                    try {
+                        cout << "Ar duomenis spausdinti ekrane - 1" << endl << "Ar duomenis spausdint faile - 2" << endl;
+                        cin >> slap;
+
+                        if (slap == 1 || slap == 2) {
+                            correctPrintInput = true;
+                            if (slap == 1)
+                                spausdint(students, galrez, median);
+                            else if (slap == 2)
+                                spausdintfaila(students, galrez, median);
+                        } else {
+                            throw "Neteisingas pasirinkimas duomenu spausdinimui.";
+                        }
+                    } catch (const char* msg) {
+                        cerr << msg << endl;
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    }
+                }
+            }
+        } catch (const char* msg) {
+            cerr << "Klaida: " << msg << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
-    } catch (const char* msg) {
-        cerr << "Klaida: " << msg << endl;
     }
     return 0;
 }
