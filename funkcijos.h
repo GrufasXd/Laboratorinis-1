b3835b479ee22503e111e6cd7e3e212ffb6f05bc
GrufasXd <147091694+GrufasXd@users.noreply.github.com>
2024-03-26 21:20:42 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -3,23 +3,23 @@
 
 #include <string>
 #include <vector>
-
+using namespace std;
 struct studentas {
     std::string vardas;
     std::string pavarde;
     std::vector<int> nd;
     int egzas;
 };
-void studrus(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<studentas>& vargsai, std::vector<studentas>& galva, const std::string& filename);
-void createfile(const std::string& filename, const int& kiekis);
-void skaitymas(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
-void rasytiranka(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
-int readInt(const std::string& prompt);
+void quickSort(vector<studentas>& students, vector<double>& galrez, vector<double>& median, int low, int high, int rusis);
+int partition(vector<studentas>& students, vector<double>& galrez, vector<double>& median, int low, int high, int rusis);
 bool isNumeric(const std::string& str);
+int readInt(const std::string& prompt);
 bool compareNames(const std::string& a, const std::string& b);
 void skaityti(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
 void spausdint(const std::vector<studentas>& students, const std::vector<double>& galrez, const std::vector<double>& median);
 void spausdintfaila(const std::vector<studentas>& students, const std::vector<double>& galrez, const std::vector<double>& median);
+void rasytiranka(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
+void skaitymas(std::vector<studentas>& students, std::vector<double>& galrez, std::vector<double>& median);
 std::string randname();
 
-#endif
+#endif // FUNKCIJOS_H
